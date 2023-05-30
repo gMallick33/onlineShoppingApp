74bc788aff4eb17c9de04288c800c1b07af51412
gyani <g.arya.mallick@gmail.com>
2023-05-30 23:32:07 +0530

    initialized the repo

---
+++ src/test/java/com/app/onlineShoppingApp/OnlineShoppingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.onlineShoppingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
